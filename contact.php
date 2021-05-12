180a578737ace7cd067562ba5213e2745d9debc4
aburke-cgcc <55819868+aburke-cgcc@users.noreply.github.com>
2021-05-11 20:00:33 -0700

    Added input box onfocus and onfocusout clearing

--- contact.php
+++ contact.php
@@ -16,7 +16,23 @@
 ?>
 <script>
     //This is javascript
-    
+    function clearInput(input){
+        //var defaultValue = input.value;
+        //cases:
+        //Empty box
+        //Filled Box with user data
+        //Default box value
+
+        if(input.value == ''){
+            input.value = input.defaultValue;
+        }
+        else if(input.value == input.defaultValue){
+            input.value='';
+        }
+        else{
+            //do nothing
+        }
+    }
 </script>
 
 <?php include('templates/header.php'); ?>
@@ -25,19 +41,19 @@
     <form>
         <div class="input-row">
             <div class="input-label"><label>First Name:</label></div>
-            <div class="input-field"><input type="text" name="first_name" value="Enter First Name"></div>
+            <div class="input-field"><input type="text" name="first_name" onfocus="clearInput(this)" onfocusout="clearInput(this)" value="Enter First Name" defaultValue="Enter First Name"></div>
         </div>
         <div class="input-row">
             <div class="input-label"><label>Last Name:</label></div>
-            <div class="input-field"><input type="text" name="last_name" value="Enter Last Name"></div>
+            <div class="input-field"><input type="text" name="last_name" onfocus="clearInput(this)" onfocusout="clearInput(this)" value="Enter Last Name" defaultValue="Enter Last Name"></div>
         </div>
         <div class="input-row">
             <div class="input-label"><label>Email Address:</label></div>
-            <div class="input-field"><input type="text" name="email" value="Enter Email Address"></div>
+            <div class="input-field"><input type="text" name="email" onfocus="clearInput(this)" onfocusout="clearInput(this)" value="Enter Email Address" defaultValue="Enter Email Address"></div>
         </div>
         <div class="input-row">
             <div class="input-label"><label>Phone Number:</label></div>
-            <div class="input-field"><input type="text" name="phone" value="Enter Phone Number"></div>
+            <div class="input-field"><input type="text" name="phone" onfocus="clearInput(this)" onfocusout="clearInput(this)" value="Enter Phone Number" defaultValue="Enter Phone Number"></div>
         </div>
         <div class="input-row">
             <div class="input-fields">
